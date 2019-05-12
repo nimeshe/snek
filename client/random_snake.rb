753a7d5428b61f3e1414388930db01a7ec75b928
Nimeshe Madanayake <nimeshe@gmail.com>
2019-05-12 18:37:32 +0800

    Commiting my Snek code that exploded my brain for a minute

--- client/random_snake.rb
+++ client/random_snake.rb
@@ -14,30 +14,113 @@ class RandomSnake
     #   length: <int>,
     #   body: [{x: <int>, y: <int>}, etc.]
     # }
-    @game_state = game_state
+    @game_state = game_state.deep_symbolize_keys
+    #puts @game_state.inspect
     # Map is a 2D array of chars.  # represents a wall and '.' is a blank tile.
     # The map is fetched once - it does not include snake positions - that's in game state.
     # The map uses [y][x] for coords so @map[0][0] would represent the top left most tile
     @map = map
     @our_snake = our_snake
-    @current_position = @our_snake.fetch("head")
+    #@our_snake_last_move = last_move
+    @our_snake_current_position = @our_snake.fetch(:head)
+    @our_snake_current_position_y = @our_snake_current_position.fetch(:y)
+    @our_snake_current_position_x = @our_snake_current_position.fetch(:x)
+    @our_snake_body = @our_snake.fetch(:body)
+    @all_snakes = @game_state.fetch(:alive_snakes)
+
+    @all_snake_heads = []
+    @all_snake_bodies = []
+    
+    @all_snakes.each do |snake|
+      if snake.fetch(:id) != @our_snake.fetch(:id)
+        @all_snake_heads << snake.fetch(:head)
+      end
+
+      snake.fetch(:body).each do |snake_body_location|
+        @all_snake_bodies << snake_body_location
+      end
+    end
+
+    @all_snake_locations = @all_snake_heads + @all_snake_bodies
+    @unsafe_locations = []
+
+    @all_snake_locations.each do |snake_location|
+      snake_location_y = snake_location.fetch(:y)
+      snake_location_x = snake_location.fetch(:x)
+
+      @unsafe_locations << {"y" => snake_location_y, "x" => snake_location_x + 1}
+      @unsafe_locations << {"y" => snake_location_y, "x" => snake_location_x - 1}
+      @unsafe_locations << {"y" => snake_location_y, "x" => snake_location_x}
+      @unsafe_locations << {"y" => snake_location_y + 1, "x" => snake_location_x}
+      @unsafe_locations << {"y" => snake_location_y - 1, "x" => snake_location_x}
+    end
+
   end
 
   def get_intent
     # Let's evaluate a random move
-    possible_moves = ["N", "S", "E", "W"].shuffle
-
-    # Note we should probably avoid walls, or other snakes hey...
-    # An exercise for the reader!
+    possible_moves = ["N", "E", "W", "S"]
+    puts "1" + possible_moves.to_s
+    # Rejecting possible moves that would bump into unsafe locations
     possible_moves.reject!{|possible_intent|
-      @our_snake.fetch(:body).include?(next_position(possible_intent).with_indifferent_access)
+      @unsafe_locations.include?(next_position(possible_intent).with_indifferent_access)
     }
 
+    puts "2" + possible_moves.to_s
+    # Rejecting possible moves that would bump into walls
+    possible_moves.reject!{|possible_intent|
+
+      case possible_intent
+      when 'N'
+        #reject if something is blocking in North Side
+        next_north = @map[@our_snake_current_position_y - 1][@our_snake_current_position_x]
+        case next_north
+        when '#'
+          true
+        else
+          false
+      end
+      when 'E'
+        #reject if something is blocking in East Side
+        next_east = @map[@our_snake_current_position_y ][@our_snake_current_position_x + 1]
+        case next_east
+        when '#'
+          true
+        else
+          false
+        end
+      when 'W'
+        #reject if something is blocking in West Side
+        next_west = @map[@our_snake_current_position_y ][@our_snake_current_position_x - 1]
+        case next_west
+        when '#'
+          true
+        else
+          false
+        end
+      when 'S'
+        #reject if something is blocking in South Side
+        next_south = @map[@our_snake_current_position_y  + 1][@our_snake_current_position_x]
+        case next_south
+        when '#'
+          true
+        else
+          false
+        end
+      end
+    }
+    puts "3" + possible_moves.to_s
     if possible_moves.empty?
       # Doh - we're dead anyway
       "N"
     else
+      #if (@our_snake_last_move !=nil && possible_moves.include?(@our_snake_last_move))
+      #  @our_snake_last_move
+      #  puts "Making the same move as last time " + @our_snake_last_move.to_s
+      #else
       possible_moves.first
+      #  puts "Making the first of the possible moves " + possible_moves.first.to_s
+      #end
     end
   end
 
@@ -45,10 +128,12 @@ class RandomSnake
 
   def next_position(possible_intent)
     case possible_intent
-    when 'N' then {y: @current_position.fetch(:y) - 1, x: @current_position.fetch(:x)}
-    when 'S' then {y: @current_position.fetch(:y) + 1, x: @current_position.fetch(:x)}
-    when 'E' then {y: @current_position.fetch(:y),     x: @current_position.fetch(:x) + 1}
-    when 'W' then {y: @current_position.fetch(:y),     x: @current_position.fetch(:x) - 1}
-    end
+    when 'N' then {"y" => @our_snake_current_position_y  - 1, "x" => @our_snake_current_position_x}
+    when 'S' then {"y" => @our_snake_current_position_y  + 1, "x" => @our_snake_current_position_x}
+    when 'E' then {"y" => @our_snake_current_position_y ,     "x" => @our_snake_current_position_x + 1}
+    when 'W' then {"y" => @our_snake_current_position_y ,     "x" => @our_snake_current_position_x - 1}
+    end.with_indifferent_access
   end
+
+
 end
--- client/runner.rb
+++ client/runner.rb
@@ -7,12 +7,13 @@ require 'active_support/core_ext/hash/indifferent_access'
 require_relative "./util/client.rb"
 require_relative "./random_snake.rb"
 
-SNEK_HOST = ENV.fetch("SNEK_HOST") { "localhost:3000" }
+SNEK_HOST = ENV.fetch("SNEK_HOST") { "10.0.1.11:8080" }
 
 $client = Client.new("http://#{SNEK_HOST}")
 
 @snake_id = nil
 @auth_token = nil
+#@last_move = nil
 
 EventMachine.run do
   uri = "ws://#{SNEK_HOST}/cable"
@@ -25,7 +26,7 @@ EventMachine.run do
   }
 
   client.disconnected {
-    @snake_name, @snake_id, @auth_token, @map = nil
+    @snake_name, @snake_id, @auth_token, @map = nil, @last_move = nil
     puts "Doh - disconnected - no snek running at #{SNEK_HOST}"
 
     sleep 1
@@ -40,17 +41,22 @@ EventMachine.run do
       game_state = payload.fetch("message").with_indifferent_access
 
       my_snake = game_state.fetch("alive_snakes").detect{|snake| snake.fetch("id") == @snake_id }
-
+      
       if !my_snake
         # Oh no - there is no my_snake.  Let's make one
-        @snake_name = "#{`whoami`.chomp} #{rand(123123)}"
+        @snake_name = "(-|-)<<NimmityNoms>>"
         puts "Making a new snake: #{@snake_name}"
         response = $client.register_snake(@snake_name)
         @snake_id = response.fetch("snake_id")
         @auth_token = response.fetch("auth_token") # Auth token is required to authenticate moves for our snake
       else
         # Yay - my_snake lives on - Let's get a move
+        time = Time.now
+        #puts "Last Move Before Move : " + @last_move.to_s
         move = RandomSnake.new(my_snake, game_state, @map).get_intent
+        #puts "Time to get move : " + (Time.now - time).to_s
+        #@last_move = move
+        #puts "Last Move After Move: " + @last_move.to_s
         puts "Snake is at: #{my_snake.fetch(:head)} - Moving #{@snake_name} #{move}"
         $client.set_intent(@snake_id, move, @auth_token)
       end
